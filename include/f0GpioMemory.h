0d9aaaf72b6e7058db475c6cdbfdbc2d3a87198d
ma-renaud <ma.renaud@slashvoid.com>
2017-08-01 09:45:24 -0400

    Remove IGpioMemory interface because it need to be a plain old type for memory mapping.

--- include/f0GpioMemory.h
+++ include/f0GpioMemory.h
@@ -1,28 +1,49 @@
 #ifndef F0GPIOMEMORY_H
 #define F0GPIOMEMORY_H
 
-#include "IGpioMemory.h"
+#include <cstdint>
 
 typedef uint32_t volatile device_register;
 
-class F0GpioMemory : public IGpioMemory
+enum class Pins : uint16_t
 {
-   public:
-      F0GpioMemory() = default;
-      ~F0GpioMemory() = default;
-      void toggle(Pins pin) override;
+    PIN_0   = 0x0001,
+    PIN_1   = 0x0002,
+    PIN_2   = 0x0004,
+    PIN_3   = 0x0008,
+    PIN_4   = 0x0010,
+    PIN_5   = 0x0020,
+    PIN_6   = 0x0040,
+    PIN_7   = 0x0080,
+    PIN_8   = 0x0100,
+    PIN_9   = 0x0200,
+    PIN_10  = 0x0400,
+    PIN_11  = 0x0800,
+    PIN_12  = 0x1000,
+    PIN_13  = 0x2000,
+    PIN_14  = 0x4000,
+    PIN_15  = 0x8000,
+    PIN_All = 0xFFFF
+};
 
-   protected:
-      device_register MODER;
-      device_register OTYPER;
-      device_register OSPEEDR;
-      device_register PUPDR;
-      device_register IDR;
-      device_register ODR;
-      device_register BSRR;
-      device_register LCKR;
-      device_register AFR[2];
-      device_register BRR;
+class F0GpioMemory
+{
+public:
+    F0GpioMemory() = default;
+    ~F0GpioMemory() = default;
+    void toggle(Pins pin);
+
+protected:
+    device_register MODER;
+    device_register OTYPER;
+    device_register OSPEEDR;
+    device_register PUPDR;
+    device_register IDR;
+    device_register ODR;
+    device_register BSRR;
+    device_register LCKR;
+    device_register AFR[2];
+    device_register BRR;
 };
 
 #endif //F0GPIOMEMORY_H
--- test/src/f0GpioTest.cpp
+++ test/src/f0GpioTest.cpp
@@ -1,35 +1,26 @@
 #include <c++/bits/unique_ptr.h>
 #include "gmock/gmock.h"
-#include "IGpioMemory.h"
+#include "f0GpioMemory.h"
 
 using namespace testing;
-
-class GpioMemoryDouble: public IGpioMemory
-{
-   public:
-      GpioMemoryDouble() = default;
-      ~GpioMemoryDouble() = default;
-
-      void toggle(Pins pin) override{};
-};
 
 class F0Gpio
 {
    public:
-      explicit F0Gpio(std::unique_ptr<IGpioMemory> _gpio_memory): gpio_memory(std::move(_gpio_memory))
+      explicit F0Gpio(std::unique_ptr<F0GpioMemory> _gpio_memory): gpio_memory(std::move(_gpio_memory))
       {
 
       }
       ~F0Gpio() = default;
 
    private:
-      std::unique_ptr<IGpioMemory> gpio_memory;
+      std::unique_ptr<F0GpioMemory> gpio_memory;
 };
 
 class GpioGroup : public Test
 {
    public:
-      std::unique_ptr<IGpioMemory> gpio_memory = std::unique_ptr<IGpioMemory>(new GpioMemoryDouble());
+      std::unique_ptr<F0GpioMemory> gpio_memory = std::make_unique<F0GpioMemory>();
       std::unique_ptr<F0Gpio> gpio;
 
       void SetUp() override
