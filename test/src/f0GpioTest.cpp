d34086740673319e2e388ca0c10ddcef2a2b7d3c
Marc-André Renaud <ma.renaud@slashvoid.com>
2020-08-20 20:23:26 -0400

    Fix unit tests

--- test/src/f0GpioTest.cpp
+++ test/src/f0GpioTest.cpp
@@ -4,34 +4,30 @@
 
 using namespace testing;
 
-class F0Gpio : public IGpio
-{
-   public:
-      F0Gpio() = default;
-      ~F0Gpio() = default;
+class F0Gpio : public IGpio {
+public:
+  F0Gpio() = default;
+  ~F0Gpio() = default;
 
-      GPIO_PinState readPin(GPIO_Pin pin) override {gpio_memory->readPin(pin);}
-      void writePin(GPIO_Pin pin, GPIO_PinState PinState) override {}
-      void togglePin(GPIO_Pin pin) override {gpio_memory->toggle(pin);}
+  GPIO_PinState readPin(GPIO_Pin pin) override { return gpio_memory->readPin(pin); }
+  void writePin(GPIO_Pin pin, GPIO_PinState PinState) override {}
+  void togglePin(GPIO_Pin pin) override { gpio_memory->toggle(pin); }
 
-   private:
-      std::unique_ptr<F0GpioMemory> gpio_memory = std::make_unique<F0GpioMemory>();
+private:
+  std::unique_ptr<F0GpioMemory> gpio_memory = std::make_unique<F0GpioMemory>();
 };
 
-class GpioGroup : public Test
-{
-   public:
-      std::unique_ptr<F0Gpio> gpio;
+class GpioGroup : public Test {
+public:
+  std::unique_ptr<F0Gpio> gpio;
 
-      void SetUp() override
-      {
-         gpio = std::make_unique<F0Gpio>();
-      }
+  void SetUp() override {
+    gpio = std::make_unique<F0Gpio>();
+  }
 
-      void TearDown() override
-      {
-         gpio.reset();
-      }
+  void TearDown() override {
+    gpio.reset();
+  }
 };
 
 /*TEST_F(GpioGroup, TestTest)
