6d18e0e75ef201f652f48b7fe6b956004c3ccb69
Marc-André Renaud <ma.renaud@slashvoid.com>
2020-08-26 08:41:53 -0400

    Circular buffer completed

--- includes/circular_buffer.h
+++ includes/circular_buffer.h
@@ -1,15 +1,13 @@
-/***************************************************************************************
-*    Title: The design and implementation of a lock-free ring-buffer with contiguous reservations
-*    Author: James Munns & Andrea Lattuada
-*    Date: 2019/06/03
-*    Availability: https://ferrous-systems.com/blog/lock-free-ring-buffer/
-***************************************************************************************/
-
 #ifndef CIRCULARBUFFER_H
 #define CIRCULARBUFFER_H
 
 #include <array>
+#include <atomic>
 #include <cstdint>
+
+// Lockfree circular buffer
+// Can only support one producer and one consumer
+// One space is lost for the lookahead -> SIZE+1
 
 template<class T, std::size_t SIZE>
 class CircularBuffer {
@@ -17,34 +15,46 @@ public:
   CircularBuffer() = default;
   ~CircularBuffer() = default;
 
-  uint8_t push(T data){
+  int8_t push(T data){
     std::size_t next;
 
-    next = (head + 1) % SIZE;
+    next = (head + 1) % (SIZE+1);
     if (next == tail) // Full
-      return -1;
+      return -1;  //fail
 
     buffer[head] = data;
     head = next;
-    return 0;
+    buf_size++;
+    return 0;  //success
   }
 
-  uint8_t pop(T &data) {
+  int8_t pop(T &data) {
     std::size_t next;
 
     if (head == tail)  // no data
-      return -1;
+      return -1;  //fail
 
-    next = (tail + 1) % SIZE;;
+    next = (tail + 1) % (SIZE+1);
 
-    *data = buffer[tail];  // Read data and then move
-    tail = next;              // tail to next offset.
-    return 0;  // return success to indicate successful push.
+    data = buffer[tail];
+    tail = next;
+    buf_size--;
+    return 0;  //success
   }
+
+  std::size_t size() {
+    return buf_size;
+  }
+
+  std::size_t available() {
+    return SIZE - buf_size;
+  }
+
 private:
-  std::size_t head {0};
-  std::size_t tail {0};
-  std::array<std::uint8_t, SIZE> buffer;
+  std::atomic_size_t head {0};
+  std::atomic_size_t tail {0};
+  std::atomic_size_t buf_size {0};
+  std::array<std::uint8_t, SIZE+1> buffer;
 };
 
 #endif //CIRCULARBUFFER_H
--- test/sources/circular_buffer_test.cpp
+++ test/sources/circular_buffer_test.cpp
@@ -5,88 +5,38 @@
 
 TEST_CASE("Circular buffer", "[circular_buffer]") {
   const size_t BUFFER_SIZE = 100;
-  cbuf_handle_t cbuf = circular_buffer_init(BUFFER_SIZE);
+  CircularBuffer<unsigned char, BUFFER_SIZE> cbuf;
 
   SECTION("Add one char") {
-    REQUIRE(circular_buffer_put(cbuf, 'a') == 0);
-    REQUIRE(circular_buf_size(cbuf) == 1);
-    REQUIRE(circular_buf_available(cbuf) == BUFFER_SIZE-1);
+    REQUIRE(cbuf.push('a') == 0);
+    REQUIRE(cbuf.size() == 1);
+    REQUIRE(cbuf.available() == BUFFER_SIZE-1);
   }
 
-  SECTION("Add one char with invalid buffer") {
-    circular_buffer_free(&cbuf);
-    REQUIRE(circular_buffer_put(cbuf, 'a') == -1);
+  SECTION("Add one char in full buffer") {
+    CircularBuffer<unsigned char, 1> new_cbuf;
+    REQUIRE(new_cbuf.push('a') == 0);
+    REQUIRE(new_cbuf.push('a') == -1);
+    REQUIRE(new_cbuf.size() == 1);
+    REQUIRE(new_cbuf.available() == 0);
   }
 
   SECTION("Get one char") {
     unsigned char expected = 'a';
-    circular_buffer_put(cbuf, expected);
+    cbuf.push(expected);
 
-    unsigned char test = 0;
-    REQUIRE(circular_buffer_get(cbuf, &test) == 0);
-    REQUIRE(test == expected);
-  }
-
-  SECTION("Get one char with invalid buffer") {
-    circular_buffer_free(&cbuf);
-    unsigned char test = 0;
-    REQUIRE(circular_buffer_get(cbuf, &test) == -1);
+    unsigned char popped = 0;
+    REQUIRE(cbuf.pop(popped) == 0);
+    REQUIRE(popped == expected);
   }
 
   SECTION("Get two char with one available") {
     unsigned char expected = 'a';
-    circular_buffer_put(cbuf, expected);
-    circular_buf_size(cbuf);
+    cbuf.push(expected);
 
-    unsigned char test = 0;
-    REQUIRE(circular_buffer_get(cbuf, &test) == 0);
-    REQUIRE(test == expected);
-    REQUIRE(circular_buffer_get(cbuf, &test) == -1);
+    unsigned char popped = 0;
+    REQUIRE(cbuf.pop(popped) == 0);
+    REQUIRE(popped == expected);
+    REQUIRE(cbuf.pop(popped) == -1);
   }
-
-  SECTION("Add four char in a two space buffer with overwrite") {
-    const size_t NEW_BUFFER_SIZE = 2;
-    circular_buffer_free(&cbuf);
-    cbuf = circular_buffer_init(NEW_BUFFER_SIZE);
-
-    REQUIRE(circular_buffer_put_over(cbuf, 'a') == 0);
-    REQUIRE(circular_buffer_put_over(cbuf, 'b') == 0);
-    REQUIRE(circular_buffer_put_over(cbuf, 'c') == 0);
-    REQUIRE(circular_buffer_put_over(cbuf, 'd') == 0);
-    REQUIRE(circular_buf_size(cbuf) == 2);
-    REQUIRE(circular_buf_full(cbuf) == 1);
-
-    unsigned char test = 0;
-    REQUIRE(circular_buffer_get(cbuf, &test) == 0);
-    REQUIRE(test == 'c');
-    REQUIRE(circular_buffer_get(cbuf, &test) == 0);
-    REQUIRE(test == 'd');
-    REQUIRE(circular_buffer_get(cbuf, &test) == -1);
-  }
-
-  SECTION("Resize buffer from existing one") {
-    const size_t INIT_BUFFER_SIZE = 5;
-    const size_t NEW_BUFFER_SIZE = 10;
-    circular_buffer_free(&cbuf);
-    cbuf = circular_buffer_init(5);
-    for(int i=97; i < 97+INIT_BUFFER_SIZE; i++)
-      circular_buffer_put(cbuf, i);
-
-    REQUIRE(circular_buffer_put(cbuf, 'a') == -1);
-
-    cbuf_handle_t new_cbuf = circular_buffer_resize(cbuf, NEW_BUFFER_SIZE);
-    circular_buffer_free(&cbuf);
-    cbuf = new_cbuf;
-
-    for(int i=97+INIT_BUFFER_SIZE; i < 97+INIT_BUFFER_SIZE+3; i++)
-      circular_buffer_put(cbuf, i);
-
-    unsigned char test = 0;
-    for(int i=0; i < INIT_BUFFER_SIZE+3; i++) {
-      circular_buffer_get(cbuf, &test);
-      REQUIRE(test == 97 + i);
-    }
-  }
-
-  circular_buffer_free(&cbuf);
 }
