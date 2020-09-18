c5158e2c99d0ae4132f40750d40e89d838f1252c
Marc-André Renaud <ma.renaud@slashvoid.com>
2020-09-17 21:29:17 -0400

    Fix console inclusion

--- cmake/console.cmake
+++ cmake/console.cmake
@@ -3,17 +3,16 @@ CPMAddPackage(
   NAME console-cpp
   GIT_REPOSITORY https://gitlab.com/christian-dsa/console.git
   GIT_TAG develop
-  SOURCE_DIR ${CMAKE_SOURCE_DIR}/console
   DOWNLOAD_ONLY True
   UPDATE_DISCONNECTED ON
 )
 
 list(APPEND ${PROJECT_NAME}_INC_DIRECTORIES
-  ${CMAKE_CURRENT_SOURCE_DIR}/console/src
+  ${console-cpp_SOURCE_DIR}/src
 )
 
 list(APPEND ${PROJECT_NAME}_SOURCES
-  ${CMAKE_CURRENT_SOURCE_DIR}/console/src/command_parser.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/console/src/command_table.cpp
-  ${CMAKE_CURRENT_SOURCE_DIR}/console/src/console.cpp
+  ${console-cpp_SOURCE_DIR}/src/command_parser.cpp
+  ${console-cpp_SOURCE_DIR}/src/command_table.cpp
+  ${console-cpp_SOURCE_DIR}/src/console.cpp
 )
